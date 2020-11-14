108376e6ef71b11b857afb671cc296f3f39bf8dd
HARSH JAIN <jainharsh270@gmail.com>
2020-11-14 15:09:09 +0530

    reverse string

---
+++ two_pointers/reverseString.java
@@ -0,0 +1,16 @@
+// #344 Reverse String
+
+class Solution {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        char temp;
+        while(i<j){
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
