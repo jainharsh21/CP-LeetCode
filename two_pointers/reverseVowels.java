1b5173fda460fcd124e5b807798bf7fb42094360
HARSH JAIN <jainharsh270@gmail.com>
2020-11-20 14:15:04 +0530

    formatting

--- two_pointers/reverseVowels.java
+++ two_pointers/reverseVowels.java
@@ -4,12 +4,11 @@
 class Solution {
     public boolean isVowel(char c)
     {
-        if(c=='a' || c=='A' || c=='e' || c=='E' || c=='i' || c=='I' || c=='o' || c=='O' ||                c=='u' || c=='U')  
+        if(c=='a' || c=='A' || c=='e' || c=='E' || c=='i' || c=='I' || c=='o' || c=='O' || c=='u' || c=='U')  
             return true;
         return false;  
     }   
     public String reverseVowels(String s) {
-                
         char[] chars = s.toCharArray();
         int i = 0;
         int j = chars.length -1;
@@ -20,12 +19,11 @@ class Solution {
             while(i<j && !isVowel(chars[j])){
                 j--;
             }
-
-                char temp = chars[i];
-                chars[i] = chars[j];
-                chars[j] = temp;
-                i++;
-                j--;
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+            i++;
+            j--;
         }
         return new String(chars);
     }
