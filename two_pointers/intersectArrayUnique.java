1e476a3fa41a8c209b0b6d1905bf060958ca24fc
HARSH JAIN <jainharsh270@gmail.com>
2020-11-20 13:55:20 +0530

    intersection of two arrays unique

---
+++ two_pointers/intersectArrayUnique.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<Integer>();
+        Set<Integer> intersect = new HashSet<Integer>();
+        int i=0;
+        for(i=0;i<nums1.length;i++){
+            set.add(nums1[i]);
+        }
+        for(int j=0;j<nums2.length;j++){
+            if(set.contains(nums2[j])){
+                intersect.add(nums2[j]);
+            }
+        }
+        i=0;
+        int[] output = new int[intersect.size()];
+        for(Integer num:intersect){
+            output[i++] = num;
+        }
+        return output;
+    }
+}
