0a9a6195ec3e4489327f358870e39b7617749b51
HARSH JAIN <jainharsh270@gmail.com>
2020-11-21 13:16:51 +0530

    backspace compare

--- two_pointers/backspaceCompare.java
+++ two_pointers/backspaceCompare.java
@@ -1,15 +1,33 @@
+// 844. Backspace String Compare
+
+// func to generate string from StringBuilder after backspacing...and then check equals and return.
+
 class Solution {
     public boolean backspaceCompare(String S, String T) {
-        int i=0;
-        String s1="",s2="";
-        for(char c:S.toCharArray()){
-            if(c=='#' && s1.length() != 0) s1 = s1.substring(0, s1.length()-1);
-            else if(c!='#') s1 += c;
+        
+        return genString(new StringBuilder(S)).equals(genString(new StringBuilder(T)));
+    }
+    
+    private String genString(StringBuilder s)
+    {
+        int i = 0;
+        
+        while (i < s.length())
+        {
+            if (s.charAt(i) == '#')
+            {
+                s.deleteCharAt(i);
+                
+                if (i-1 >= 0)
+                {
+                    s.deleteCharAt(i-1);
+                    i--;
+                }
+            }
+            else
+                i++;
         }
-        for(char c:T.toCharArray()){
-            if(c=='#' && s2.length() != 0) s2 = s2.substring(0, s2.length()-1);
-            else if(c!= '#') s2 += c;
-        }
-        return s1.equals(s2);
+        
+        return s.toString();
     }
 }
