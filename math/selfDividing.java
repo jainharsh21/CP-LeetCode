9e68fdaad3d81fbedaf253f0e86b49dfba08029f
HARSH JAIN <jainharsh270@gmail.com>
2021-04-08 13:21:37 +0530

    seld dividing number

--- math/selfDividing.java
+++ math/selfDividing.java
@@ -18,3 +18,23 @@ class Solution {
         return res;
     }
 }
+
+// without using toString
+
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i=left;i<=right;i++){
+            int temp = i;
+            while(temp>0){
+               int d = temp % 10;
+               if(d==0 || i%d!=0)
+                    break;
+               temp =temp/10;
+            }
+            if(temp==0)
+                res.add(i);
+        }
+        return res;
+    }
+}
