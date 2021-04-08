2b6ca24c5295e688685b4cedd6f36894ab15c8b5
HARSH JAIN <jainharsh270@gmail.com>
2021-04-08 11:59:02 +0530

    good pairs

---
+++ math/goodPair.java
@@ -0,0 +1,14 @@
+// 1512. Number of Good Pairs
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int result = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j])
+                    result++;
+            }
+        }
+        return result;
+    }
+}
