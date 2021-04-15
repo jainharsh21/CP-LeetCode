98aad3894ada41120e69d56c9d267336ce120ee2
HARSH JAIN <jainharsh270@gmail.com>
2021-04-15 11:29:02 +0530

    Single Number

---
+++ array/singleNumber.java
@@ -0,0 +1,20 @@
+// 136. Single Number
+
+// Bruteforce
+class Solution {
+    int countOccurence(int[] arr,int n){
+        int count =0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==n)
+                count++;
+        }
+        return count;
+    }
+    public int singleNumber(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            if(countOccurence(nums,nums[i])==1)
+                return nums[i];
+        }
+        return -1;
+    }
+}
