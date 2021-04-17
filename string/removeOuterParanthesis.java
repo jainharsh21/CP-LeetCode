6ae11c8b14b6cb4ef664ffa19b89312a4cd72892
HARSH JAIN <jainharsh270@gmail.com>
2021-04-17 11:53:59 +0530

    remove outer parentheses

---
+++ string/removeOuterParanthesis.java
@@ -0,0 +1,15 @@
+// 1021. Remove Outermost Parentheses
+
+class Solution {
+    public String removeOuterParentheses(String S) {
+        StringBuilder res = new StringBuilder() ;
+        int opened = 0;
+        for(char c : S.toCharArray()){
+            if(c=='(' && opened++ > 0)
+                res.append(c);
+            if(c==')' && opened-- > 1)
+                res.append(c);
+        }
+        return res.toString();
+    }
+}
