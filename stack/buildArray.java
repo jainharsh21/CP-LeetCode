c226e70df0b1d535b3fd1c3ad481d3f896c9a643
HARSH JAIN <jainharsh270@gmail.com>
2021-04-19 11:02:17 +0530

    build array

---
+++ stack/buildArray.java
@@ -0,0 +1,17 @@
+// 1441. Build an Array With Stack Operations
+
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> result = new ArrayList<>();
+        int j=0;
+        for(int i=1;i<=n && j<target.length;i++){
+            result.add("Push");
+            if(target[j]==i){
+                j++;
+            }else{
+                result.add("Pop");
+            }
+        }
+        return result;
+    }
+}
