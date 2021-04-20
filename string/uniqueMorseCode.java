73adc8eb5bc7a0908ba64b2f4dda96ff1d4bf931
HARSH JAIN <jainharsh270@gmail.com>
2021-04-20 15:31:20 +0530

    unique morse code representation

---
+++ string/uniqueMorseCode.java
@@ -0,0 +1,15 @@
+// 804. Unique Morse Code Words
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] d = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",           "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-",           ".--", "-..-", "-.--", "--.."};
+        HashSet<String> s = new HashSet<>();
+        for (String w : words) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < w.length(); ++i)
+                sb.append(d[w.charAt(i) - 'a']);
+            s.add(sb.toString());
+        }
+        return s.size();
+    }
+}
